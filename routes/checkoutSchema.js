fa58d455e37c48691a0e4b57f53dd1aa2c876aec
arnel <arne.landwehr@innoq.com>
2019-12-06 10:33:00 +0100

    SWDBECOM-29 add checkout resource to mock

---
+++ routes/checkoutSchema.js
@@ -0,0 +1,80 @@
+module.exports.checkoutSchema = {
+    $schema: "http://json-schema.org/draft-04/schema#",
+    type: "object",
+    properties: {
+        productId: {
+            type: "integer"
+        },
+        customer_company: {
+            type: "string"
+        },
+        customer_salutation: {
+            type: "string"
+        },
+        customer_firstname: {
+            type: "string"
+        },
+        customer_lastname: {
+            type: "string"
+        },
+        customer_street: {
+            type: "string"
+        },
+        customer_zip: {
+            type: "string"
+        },
+        customer_city: {
+            type: "string"
+        },
+        customer_country: {
+            type: "string"
+        },
+        customer_email: {
+            type: "string"
+        },
+        device_manufacturer: {
+            type: "string"
+        },
+        device_model: {
+            type: "string"
+        },
+        device_class: {
+            type: "string"
+        },
+        device_purchase_price: {
+            type: "number"
+        },
+        device_purchase_date: {
+            type: "string"
+        },
+        device_condition: {
+            type: "integer"
+        },
+        payment_type: {
+            type: "string"
+        },
+        terms_and_conditions_accepted: {
+            type: "boolean"
+        }
+    },
+    required: [
+        "productId",
+        "customer_company",
+        "customer_salutation",
+        "customer_firstname",
+        "customer_lastname",
+        "customer_street",
+        "customer_zip",
+        "customer_city",
+        "customer_country",
+        "customer_email",
+        "device_manufacturer",
+        "device_model",
+        "device_class",
+        "device_purchase_price",
+        "device_purchase_date",
+        "device_condition",
+        "payment_type",
+        "terms_and_conditions_accepted"
+    ]
+};
--- routes/policies.js
+++ routes/policies.js
@@ -1,5 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const validate = require('express-jsonschema').validate;
+const checkoutSchema = require('./checkoutSchema').checkoutSchema;
+const router = express.Router();
+
 
 const phone11 = {
     "id": 11,
@@ -47,7 +50,7 @@ const phone11 = {
             "document_link": "https://stage-api.wertgarantie.com/download/76c4e67c-89bf-4bd4-a1d2-b6470ed07629"
         }
     ]
-}
+};
 
 const phone14 = {
     "id": 14,
@@ -95,9 +98,9 @@ const phone14 = {
             "document_link": "https://stage-api.wertgarantie.com/download/49b77b3d-a949-4fa9-8c51-34edadfa959d"
         }
     ]
-}
+};
 
-var jsonPayloadPhones = [
+const jsonPayloadPhones = [
     phone11,
     phone14
 ];
@@ -143,7 +146,7 @@ const bike21 = {
             "document_link": "https://stage-api.wertgarantie.com/download/76c4e67c-89bf-4bd4-a1d2-b6470ed07629"
         }
     ]
-}
+};
 
 const bike24 = {
     "id": 24,
@@ -188,9 +191,9 @@ const bike24 = {
             "document_link": "https://stage-api.wertgarantie.com/download/49b77b3d-a949-4fa9-8c51-34edadfa959d"
         }
     ]
-}
+};
 
-var jsonPayloadBikes = [
+const jsonPayloadBikes = [
     bike21,
     bike24
 ];
@@ -202,17 +205,17 @@ const returnProductMap = {
     "bb3a615d-e92f-4d24-a4cc-22f8a87fc544": jsonPayloadPhones,
     "ebfb2d44-4ff8-4579-9cc0-0a3ccb8d6f2d": jsonPayloadPhones,
     "bike": jsonPayloadBikes
-}
+};
 
 const allProductsByDeviceClass = {
     "1dfd4549-9bdc-4285-9047-e5088272dade": {
         "11": phone11,
         "14": phone14
-    }, 
+    },
     "2dc16d28-c7af-4e19-9494-1659e1c27201": {
         "11": phone11,
         "14": phone14
-    }, 
+    },
     "41ade4ba-5f24-4321-b706-fa6d05d75a73": {
         "11": phone11,
         "14": phone14
@@ -229,13 +232,13 @@ const allProductsByDeviceClass = {
         "21": bike21,
         "24": bike24
     }
-}
+};
 
 function getProductsByDeviceClass(deviceClass) {
-    var products = returnProductMap[deviceClass];
+    const products = returnProductMap[deviceClass];
     if (products) {
         return products;
-    } 
+    }
     return jsonPayloadBikes;
 }
 
@@ -243,7 +246,7 @@ function getProductsByDeviceClass(deviceClass) {
 https://api.wertgarantie.com/api/v1/product-offers?device_class=04854bfa-1a02-4b44-b981-46f7ead8bb7e&device_purchase_price=800&device_purchase_date=2018-09-01
 */
 router.get('/api/v1/dummy-product-offers', function (req, res) {
-    if(!(req.query.device_class && req.query.device_purchase_price && req.query.device_purchase_date)) {
+    if (!(req.query.device_class && req.query.device_purchase_price && req.query.device_purchase_date)) {
         throw new Error("Insufficient query parameters");
     }
 
@@ -260,21 +263,21 @@ router.get('/api/v1/dummy-product-offers', function (req, res) {
 
 router.get('/api/v1/dummy-product', function (req, res) {
     if (!(req.query.device_class && req.query.product_id)) {
-        res.status(400).send({ error: "insufficient query params: device_class and product_id must be provided" });
+        res.status(400).send({error: "insufficient query params: device_class and product_id must be provided"});
     }
-    var productsForDeviceClass = allProductsByDeviceClass[req.query.device_class];
+    const productsForDeviceClass = allProductsByDeviceClass[req.query.device_class];
     if (!productsForDeviceClass) {
-        res.status(400).send( {error: "Device class '" + req.query.device_class + "' unknown."} );
+        res.status(400).send({error: "Device class '" + req.query.device_class + "' unknown."});
     }
-    var product = productsForDeviceClass[req.query.product_id];
+    const product = productsForDeviceClass[req.query.product_id];
     if (!product) {
-        res.status(400).send( {error: "No product for device class '" + req.query.device_class + "' unknown."} );
+        res.status(400).send({error: "No product for device class '" + req.query.device_class + "' unknown."});
     }
     res.send(product);
 });
 
 router.get('/api/v1/product-offers', function (req, res) {
-    if(!(req.query.device_class && req.query.device_purchase_price && req.query.device_purchase_date)) {
+    if (!(req.query.device_class && req.query.device_purchase_price && req.query.device_purchase_date)) {
         throw new Error("Insufficient query parameters");
     }
 
@@ -354,6 +357,18 @@ router.get('/api/v1/product-offers', function (req, res) {
                 ]
             }
         ]
+    });
+});
+
+
+router.post("/api/v1/products/:productId/checkout", validate({body: checkoutSchema}), function (req, res) {
+    res.send({
+        payload: {
+            contract_number: "28850277",
+            transaction_number: "28850277",
+            activation_code: "4db56dacfbhce",
+            message: "Der Versicherungsantrag wurde erfolgreich übermittelt."
+        }
     });
 });
 
